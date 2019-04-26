3854a42080fd02bf9ceb923c2c24ce27eaf7e077
Diogo Hosken <diogoolivehosken@hotmail.com>
2019-04-26 02:00:15 -0300

    iniciando um novo repositório na pasta landingPage da xiaomi e monitorei todo o processo que havia feito até agora

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE hmtl>
+<html lang="pt-br">
+	<head>
+		<meta name="viewport" content="width=device-width, user-scalable=0"/>
+		<meta charset="UTF-8">
+		<title>Xiaomi</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	</head>
+	<body>
+		<header class="container-fluid" style="background-color:#373f42;">
+			<div class="container">
+				<nav class="row">
+					<div class="col-xs-5 hLeft" style="text-align:left">
+						<a href="#"><img src="assets/images/Xiaomi-logo.jpg" style="height:30px"></a> Xiaomi
+					</div>
+					<div class="col-xs-7 hRight" style="text-align:right">
+						<ul class="redeSocial">
+							<li class="ic"><a href=""><img src="assets/images/facebook.png" style="height: 30px;"></a></li>
+							<li class="ic"><a href=""><img src="assets/images/twitter.png" style="height: 30px;"></a></li>
+							<li class="ic"><a href=""><img src="assets/images/googleplus.png" style="height: 30px;"></a></li>
+							<li class="ic"><a href=""><img src="assets/images/rss.png" style="height: 30px;"></a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</header>
+
+		<article>
+		<section class="container-fluid sect1">
+			<div class="container">
+				<div class="row sect-row-1">
+					<div class="col-sm-7" style="text-align:center">
+						<div class="iframe-container">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/F_ydZi9rtrI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+					</div>
+
+					<div class="col-sm-5">
+						<h1 class="jumbotron">We've launched our product and it's awesome!!</h1><br>
+						<h4>Check out our video to get the idea of the power of our product. Get it now!
+						Be the FIRST to try our combination of fashion and efficience, all combine in one.</h4>
+						<h2>Mi Band 3</h2>
+						<button class="btn btn1"><img src="assets/images/shopping-cart-black-shape.png"> Get your own!</button>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="container-fluid sect2">
+			<div class="container">
+				<div class="row sect-row-1">
+					<div class="col-sm-5">
+						<h1>Thats our new tech developed by the most bright engineers</h1>
+						<h4>It includes heartbeating reading, bluetooth conection, wi-fi access and much more! Also combines with the apps
+						Whatsapp, Instagram, Facebook, Twitter... and much more.</h4><br>
+						<button class="btn btn1" onclick="window.location.href='https://mundoconectado.com.br/analises/v/6603/analise-xiaomi-mi-band-3-update' ">
+							Read about...
+						</button>
+					</div>
+
+					<div class="col-sm-7" style="text-align:center">
+						<img src="assets/images/miband3.png" height="315px">
+					</div>
+				</div>
+			</div>
+
+		</section>
+
+		<section class="container-fluid sect1">
+			<div class="container">
+				<div class="row sect-row-1">
+					<div class="col-sm-7" style="text-align:center">
+						<img src="assets/images/pulseiras.png" height="315px">
+					</div>
+					<div class="col-sm-5">
+						<h1>You can try different colors if you want</h1>
+						<h3>We have  new colors available this time:</h3>
+						<h4>black, grey, white, red and blue</h4>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+		</article>
+
+		<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	</body>
+</html>
